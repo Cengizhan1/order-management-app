903926ff10dd786734d657356f49f1739d2ee126
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2023-11-04 12:46:29 +0300

    ICustomerServices and IOrderServices added

---
+++ src/main/java/com/cengizhan/ordermanagement/business/services/ICustomerServices.java
@@ -0,0 +1,32 @@
+package com.cengizhan.ordermanagement.business.services;
+import java.util.List;
+
+// D: Dto
+// E: Entity
+public interface ICustomerServices<D, E> {
+
+    // Model Mapper
+    public D entityToDto(E e);
+
+    public E dtoToEntity(D d);
+
+    // C R U D
+    // CREATE
+    public D customerServiceCreate(D d);
+
+    // LIST
+    public List<D> customerServiceList();
+
+    // FIND BY
+    public D customerServiceFindById(Long id);
+
+    // UPDATE
+    public D customerServiceUpdate(Long id,D d);
+
+    // DELETE
+    public D customerServiceDeleteById(Long id);
+
+    // All DELETE
+    public D customerServiceDeleteAll();
+
+}
---
+++ src/main/java/com/cengizhan/ordermanagement/business/services/IOrderServices.java
@@ -0,0 +1,31 @@
+package com.cengizhan.ordermanagement.business.services;
+import java.util.List;
+
+// D: Dto
+// E: Entity
+public interface IOrderServices<D, E> {
+
+    // Model Mapper
+    public D entityToDto(E e);
+
+    public E dtoToEntity(D d);
+
+    // C R U D
+    // CREATE
+    public D orderServiceCreate(D d);
+
+    // LIST
+    public List<D> orderServiceList(Long id);
+
+    // FIND BY
+    public D orderServiceFindById(Long id);
+
+    // UPDATE
+    public D orderServiceUpdate(Long id,D d);
+
+    // DELETE
+    public D orderServiceDeleteById(Long id);
+
+    // All DELETE
+    public D orderServiceDeleteAll();
+}
