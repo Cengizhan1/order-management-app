44c373695e48119efa8411544b621efbf93379c6
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2023-11-04 13:22:58 +0300

    some exceptions added

---
+++ src/main/java/com/cengizhan/ordermanagement/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.cengizhan.ordermanagement.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
