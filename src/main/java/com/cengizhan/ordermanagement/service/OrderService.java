0b9f519fad609c2c92efa03cfba705be84cd1734
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-27 13:43:53 +0300

    OrderService updated

--- src/main/java/com/cengizhan/ordermanagement/service/OrderService.java
+++ src/main/java/com/cengizhan/ordermanagement/service/OrderService.java
@@ -1,111 +1,80 @@
 package com.cengizhan.ordermanagement.service;
 
+import com.cengizhan.ordermanagement.dto.request.OrderCreateRequest;
 import com.cengizhan.ordermanagement.dto.OrderDto;
+import com.cengizhan.ordermanagement.dto.request.OrderUpdateRequest;
+import com.cengizhan.ordermanagement.entity.Customer;
 import com.cengizhan.ordermanagement.entity.Order;
-import com.cengizhan.ordermanagement.repository.ICustomerRepository;
+import com.cengizhan.ordermanagement.exception.OrderNotFoundException;
 import com.cengizhan.ordermanagement.repository.IOrderRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.ArrayList;
-import java.util.Date;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class OrderService {
 
     private final IOrderRepository iOrderRepository;
-    private final ICustomerRepository iCustomerRepository;
+    private final CustomerService customerService;
 
-    public OrderService(IOrderRepository iOrderRepository, ICustomerRepository iCustomerRepository) {
+    public OrderService(IOrderRepository iOrderRepository, CustomerService customerService) {
         this.iOrderRepository = iOrderRepository;
-        this.iCustomerRepository = iCustomerRepository;
+        this.customerService = customerService;
     }
 
+    protected Order getOrder(Long id) {
+        return iOrderRepository.findById(id)
+                .orElseThrow(
+                        () -> new OrderNotFoundException("Order could not found by id: " + id)
+                );
+    }
 
-    // CREATE
-//    @Transactional // create, delete, update
-//    public OrderDto orderServiceCreate(OrderDto orderDto) {
-//        if (orderDto != null) {
-//            Order order = dtoToEntity(orderDto);
-//            order.setRelationCustomer(iCustomerRepository.findById(orderDto.getCustomerId()).get());
-//            iOrderRepository.save(order);
-//            orderDto.setId(order.getOrderId());
-//        } else {
-//            throw new NullPointerException(" OrderDto null");
-//        }
-//        return orderDto;
-//    }
-//
-//    // LIST
-//    public List<OrderDto> orderServiceList() {
-//        Iterable<Order> entityIterable=  iOrderRepository.findAll();
-//        List<OrderDto> orderDtoList = new ArrayList<>();
-//        for (Order entity : entityIterable) {
-//            OrderDto orderDto = entityToDto(entity);
-//            orderDtoList.add(orderDto);
-//        }
-//        log.info("Liste Sayısı: " + orderDtoList.size());
-//        return orderDtoList;
-//    }
-//
-//
-//    // FIND
-//    public OrderDto orderServiceFindById(Long id) {
-//        Order findOrder = null;
-//        if (id != null) {
-//            findOrder = iOrderRepository.findById(id)
-//                    .orElseThrow(() -> new ResourceNotFoundException(id + " nolu id yoktur"));
-//        } else {
-//            throw new CustomException("İd null olarak geldi");
-//        }
-//        return entityToDto(findOrder);
-//    }
-//
-//    // UPDATE
-//    @Transactional // create, delete, update
-//    public OrderDto orderServiceUpdate(Long id, OrderDto orderDto) {
-//        OrderDto orderFindDto = orderServiceFindById(id);
-//        if (orderFindDto != null) {
-//            Order order = dtoToEntity(orderFindDto);
-//            order.setTotalPrice(orderDto.getTotalPrice());
-//            order.setRelationCustomer(iCustomerRepository.findById(orderDto.getCustomerId()).get());
-//            iOrderRepository.save(order);
-//            orderDto.setId(id);
-//        }
-//        return orderDto;
-//    }
-//
-//    // DELETE
-//    @Transactional // create, delete, update
-//    public OrderDto orderServiceDeleteById(Long id) {
-//        OrderDto orderFindDto = orderServiceFindById(id);
-//        if (orderFindDto != null) {
-//            iOrderRepository.deleteById(id);
-//        }
-//        return orderFindDto;
-//    }
-//
-//
-//    @Transactional // create, delete, update
-//    public OrderDto orderServiceDeleteAll() {
-//        iOrderRepository.deleteAll();
-//        return null;
-//    }
-//
-//
-//    public List<OrderDto> orderServiceFindByCreatedDateAfter(Date date) {
-//        List<Order> orderEntities = iOrderRepository.findByCreatedDateAfter(date);
-//        log.info(date);
-//        log.info(orderEntities.size());
-//        List<OrderDto> orderDtoList = new ArrayList<>();
-//        for (Order entity : orderEntities) {
-//            OrderDto orderDto = entityToDto(entity);
-//            orderDtoList.add(orderDto);
-//        }
-//
-//        return orderDtoList;
-//    }
+    @Transactional
+    public OrderDto orderCreate(OrderCreateRequest orderCreateRequest) {
+        Customer customer = customerService.getCustomer(orderCreateRequest.customerId());
+        Order order = Order.builder()
+                .totalPrice(orderCreateRequest.totalPrice())
+                .relationCustomer(customer)
+                .createdAt(orderCreateRequest.createdAt())
+                .build();
+        iOrderRepository.save(order);
+        return OrderDto.convert(order);
+    }
 
+    public List<OrderDto> orderList() {
+        return iOrderRepository.findAll()
+                .stream()
+                .map(OrderDto::convert)
+                .toList();
+    }
 
+    public OrderDto orderFindById(Long id) {
+        return OrderDto.convert(getOrder(id));
+    }
 
+    @Transactional
+    public OrderDto orderUpdate(Long id, OrderUpdateRequest orderUpdateRequest) {
+        Order order = getOrder(id);
+        order.setTotalPrice(orderUpdateRequest.totalPrice());
+        order.setCreatedAt(orderUpdateRequest.createdAt());
+        return OrderDto.convert(iOrderRepository.save(order));
+    }
+
+    @Transactional // create, delete, update
+    public void orderDeleteById(Long id) {
+        getOrder(id); // TODO deleteById methodu mevcut olmayan bir id geldiğinde nasıl hata verir handle edilebilir mi
+        iOrderRepository.deleteById(id);
+    }
+
+    @Transactional // create, delete, update
+    public void orderDeleteAll() {
+        iOrderRepository.deleteAll();
+    }
+
+    public List<OrderDto> orderFindByCreatedDateAfter(LocalDateTime date) {
+        List<Order> orders = iOrderRepository.findByCreatedAtAfter(date);
+        return orders.stream().map(OrderDto::convert).toList();
+    }
 }
