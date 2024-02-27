bfe3b02367427f7d4fb29e0407176002aefe6377
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-27 20:46:37 +0300

    docker compose file  added

---
+++ src/test/java/com/cengizhan/ordermanagement/service/OrderServiceTest.java
@@ -0,0 +1,36 @@
+package com.cengizhan.ordermanagement.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void orderCreate() {
+    }
+
+    @Test
+    void orderList() {
+    }
+
+    @Test
+    void orderFindById() {
+    }
+
+    @Test
+    void orderUpdate() {
+    }
+
+    @Test
+    void orderDeleteById() {
+    }
+
+    @Test
+    void orderDeleteAll() {
+    }
+
+    @Test
+    void orderFindAllByCreatedAtAfter() {
+    }
+}
