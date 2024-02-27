8483140e82ead5bbd95dabe406dafbcc0e93124e
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-27 14:32:30 +0300

    CustomerController updated

--- src/main/java/com/cengizhan/ordermanagement/controller/CustomerController.java
+++ src/main/java/com/cengizhan/ordermanagement/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.cengizhan.ordermanagement.controller;
 
 import com.cengizhan.ordermanagement.dto.CustomerDto;
+import com.cengizhan.ordermanagement.dto.request.CustomerCreateRequest;
+import com.cengizhan.ordermanagement.dto.request.CustomerUpdateRequest;
 import com.cengizhan.ordermanagement.service.CustomerService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,66 +14,66 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/customer/api/v1")
+@RequestMapping("/v1/api/customer")
 public class CustomerController {
-    
+
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
-    //cREATE
-    // http://localhost:8000/customer/api/v1/create
-//    @PostMapping("/create")
-//    public ResponseEntity<?> customerApiCreate(@Valid @RequestBody CustomerDto customerDto) {
-//        return ResponseEntity.ok(customerService.customerServiceCreate(customerDto));
-//    }
-//
-//    // LIST
-//    // http://localhost:8000/customer/api/v1/list
-//    @GetMapping(value = "/list")
-//    public ResponseEntity<List<CustomerDto>> customerApiList() {
-//        return ResponseEntity.status(HttpStatus.OK).body(customerService.customerServiceList());
-//    }
-//
-//    // FIND
-//    // http://localhost:8000/customer/api/v1/find/1
-//    @GetMapping(value = "/find/{id}")
-//    public ResponseEntity<?>  customerApiFindById(@PathVariable(name = "id") Long id) {
-//        return ResponseEntity.status(200).body(customerService.customerServiceFindById(id));
-//    }
-//
-//    // UPDATE
-//    // http://localhost:8000/customer/api/v1/update/1
-//    @PutMapping(value = "/update/{id}")
-//    public ResponseEntity<?> customerApiUpdate(@PathVariable(name = "id") Long id, @Valid @RequestBody CustomerDto customerDto) {
-//        return ResponseEntity.ok().body(customerService.customerServiceUpdate(id, customerDto));
-//    }
-//
-//    // DELETE BY ID
-//    // http://localhost:8000/customer/api/v1/delete/1
-//    @DeleteMapping(value = "/delete/{id}")
-//    public ResponseEntity<?> customerApiDeleteById(@PathVariable(name = "id") Long id) {
-//        return new ResponseEntity<>(customerService.customerServiceDeleteById(id), HttpStatus.OK);
-//    }
-//
-//    // ALL DELETE
-//    // http://localhost:8000/customer/api/v1/all/delete
-//    @PostMapping(value = "/all/delete")
-//    public ResponseEntity<?> customerApiAllDelete() {
-//        return new ResponseEntity<>(customerService.customerServiceDeleteAll(), HttpStatus.OK);
-//    }
-//
-//
-//    @GetMapping("/getByNameContains")
-//    public ResponseEntity<List<CustomerDto>> getCustomersByNameContains(@RequestParam String keyword) {
-//        return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomersByNameContains(keyword));
-//    }
-//
-//
-//    @GetMapping("/getCustomersWithoutOrders")
-//    public ResponseEntity<List<CustomerDto>> getCustomersWithoutOrders() {
-//        return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomersWithoutOrders());
-//    }
+
+    // http://localhost:8000/v1/api/customer
+    @PostMapping
+    public ResponseEntity<CustomerDto> customerCreate(@Valid @RequestBody CustomerCreateRequest customerCreateRequest) {
+        return ResponseEntity.ok(customerService.customerCreate(customerCreateRequest));
+    }
+
+    // http://localhost:8000/v1/api/customer
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> customerList() {
+        return ResponseEntity.ok(customerService.customerList());
+    }
+
+    // http://localhost:8000/v1/api/customer/1
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CustomerDto>  customerFindById(@NotBlank @PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(customerService.customerFindById(id));
+    }
+
+    // http://localhost:8000/v1/api/customer/1
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> customerUpdate(@NotBlank @PathVariable(name = "id") Long id,
+                                            @Valid @RequestBody CustomerUpdateRequest customerUpdateRequest) {
+        return ResponseEntity.ok(customerService.customerUpdate(id, customerUpdateRequest));
+    }
+
+
+    // http://localhost:8000/v1/api/customer/1
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> customerDeleteById(@NotBlank @PathVariable(name = "id") Long id) {
+        customerService.customerDeleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    // http://localhost:8000/v1/api/customer/all/delete
+    @PostMapping(value = "/all/delete")
+    public ResponseEntity<Void> customerAllDelete() {
+        customerService.customerDeleteAll();
+        return ResponseEntity.ok().build();
+    }
+
+    // http://localhost:8000/v1/api/customer/getByNameContains/{keyword}
+    @GetMapping("/getByNameContains/{keyword}")
+    public ResponseEntity<List<CustomerDto>> getCustomersByNameContains(
+            @PathVariable(name = "keyword") String keyword) {
+        return ResponseEntity.ok(customerService.getCustomersByNameContaining(keyword));
+    }
+
+    // http://localhost:8000/v1/api/customer/getCustomersWithoutOrders
+    @GetMapping("/getCustomersWithoutOrders")
+    public ResponseEntity<List<CustomerDto>> getCustomersWithoutOrders() {
+        return ResponseEntity.ok(customerService.getCustomersWithoutOrders());
+    }
 }
