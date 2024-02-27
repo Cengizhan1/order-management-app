713a79874d7555b8ec758ce4a16eef1e1c6d7ab8
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-27 13:45:35 +0300

    IOrderRepositoryupdated

--- src/main/java/com/cengizhan/ordermanagement/repository/IOrderRepository.java
+++ src/main/java/com/cengizhan/ordermanagement/repository/IOrderRepository.java
@@ -3,12 +3,13 @@ package com.cengizhan.ordermanagement.repository;
 import com.cengizhan.ordermanagement.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 public interface IOrderRepository extends JpaRepository<Order,Long> {
     List<Order> findByRelationCustomerCustomerId(Long customerId);
-    List<Order> findByCreatedDateAfter(Date date);
+    List<Order> findByCreatedAtAfter(LocalDateTime date);
 
 
 }
