91fd4b000a07b33233b27513ca23d427b35e29ca
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-03-01 13:12:41 +0300

    bug fix

--- src/test/java/com/cengizhan/ordermanagement/controller/CustomerControllerTest.java
+++ src/test/java/com/cengizhan/ordermanagement/controller/CustomerControllerTest.java
@@ -8,11 +8,13 @@ import com.cengizhan.ordermanagement.entity.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -21,9 +23,13 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = WavefrontProperties.Application.class)
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "server.port=0")
-@DirtiesContext
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
 class CustomerControllerTest extends IntegrationTestSupport {
 
 
